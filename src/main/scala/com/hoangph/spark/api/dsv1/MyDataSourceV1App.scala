15ad2822e18329028e060a894ef19a4468494388
phamhuyhoang97 <hoangtb31397@gmail.com>
2021-10-24 22:35:48 +0700

    initial

---
+++ src/main/scala/com/hoangph/spark/api/dsv1/MyDataSourceV1App.scala
@@ -0,0 +1,27 @@
+package com.hoangph.spark.api.dsv1
+
+import org.apache.spark.SparkConf
+import org.apache.spark.sql.{SaveMode, SparkSession}
+
+object MyDataSourceV1App  extends App{
+    val conf = new SparkConf().setAppName("spark-custom-datasource")
+    val spark = SparkSession.builder().config(conf).master("local").getOrCreate()
+
+    val df = spark.read.format("com.hoangph.spark.api.dsv1").load("data/")
+
+    // Step 1 (Schema verification)
+    df.printSchema()
+    // Step 2 (Read data)
+    df.show()
+    // Step 3 (Write data)
+    df.write
+        .options(Map("format" -> "customFormat"))
+        .mode(SaveMode.Overwrite)
+        .format("com.hoangph.spark.ds.api.v1")
+        .save("out/")
+    // Step 4 (Column Pruning)
+    df.createOrReplaceTempView("salaries")
+    spark.sql("SELECT surname, salary FROM salaries").show()
+    // Step 5 (Filter Pushdown)
+    spark.sql("SELECT name, surname FROM salaries WHERE salary > 40000").show()
+}
